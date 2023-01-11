397dc265e1739f06202ef2eafe0b91b356a0253f
unknown <danielruizcas@gmail.com>
2023-01-10 19:58:00 -0500

    version inicial

---
+++ Juego/snake.js
@@ -0,0 +1,100 @@
+//board
+var blockSize = 25;
+var rows = 25;
+var cols = 25;
+var board;
+var context;
+
+//snake head
+var snakeX = blockSize * 2;
+var snakeY = blockSize * 18;
+
+//food
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+var gameOver = false;
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); 
+    
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update,1500/10);
+
+
+}
+function update(){
+    if (gameOver){
+        return;
+    }
+
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if(snakeX == foodX && snakeY == foodY){
+        snakeBody.push([foodX, foodY])
+        placeFood();
+    }
+
+    for (let i = snakeBody.length-1; i > 0; i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if(snakeBody.length){
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle="green";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++){
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //game over conditions
+    if (snakeX < 0 || snakeX > (cols-1)*blockSize || snakeY < 0 || snakeY > (rows-1)*blockSize){
+        gameOver = true;
+        alert("Game Over");
+    }
+
+    for (let i = 0; i < snakeBody.length; i++){
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
+}
+
+function changeDirection(e){
+    if(e.code == "ArrowUp" && velocityY != 1){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.code == "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.code == "ArrowRight" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood(){
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
